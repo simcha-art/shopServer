3b9cb1b372d965dd8ea3292efcbbf9312c23b61f
simcha <simchacop@gmail.com>
2026-07-09 11:10:25 +0300

    feat: funcs to read and write from and to the json files

---
+++ services/io.js
@@ -0,0 +1,24 @@
+import env from "dotenv";
+import fs from "fs/promises";
+
+env.config();
+const DB_BOOKS = process.env.DB_BOOKS;
+const DB_ORDERS = process.env.DB_ORDERS;
+const DB_CUSTOMERS = process.env.DB_CUSTOMERS;
+
+const readBooksFile = () => fs.readFile(DB_BOOKS, "utf8").then((data) => JSON.parse(data));
+const readOrdersFile = () => fs.readFile(DB_ORDERS, "utf8").then((data) => JSON.parse(data));
+const readCustomersFile = () => fs.readFile(DB_CUSTOMERS, "utf8").then((data) => JSON.parse(data));
+
+const writeBooksFile = (data) => fs.writeFile(DB_BOOKS, JSON.stringify(data));
+const writeOrdersFile = (data) => fs.writeFile(DB_ORDERS, JSON.stringify(data));
+const writeCustomersFile = (data) => fs.writeFile(DB_CUSTOMERS, JSON.stringify(data));
+
+export {
+    readBooksFile,
+    readOrdersFile,
+    readCustomersFile,
+    writeBooksFile,
+    writeOrdersFile,
+    writeCustomersFile,
+};
