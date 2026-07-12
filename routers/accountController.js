2c6a57cc0012ba231c3519d771ef2ddd010b859a
simcha <simchacop@gmail.com>
2026-07-12 03:02:54 +0300

    feat: func to create a new account of customer. used to update later that when user asks for customerId that not found, it will create a new customer with this id

--- routers/accountController.js
+++ routers/accountController.js
@@ -1,7 +1,10 @@
 import { findCustomer } from "../services/logic.js";
-import { readCustomersFile } from "../services/io.js";
+import {
+    readCustomersFile,
+    writeBooksFile,
+    writeCustomersFile,
+} from "../services/io.js";
 import { fail, success } from "../services/responses.js";
-
 
 export async function getBalance(req, res) {
     try {
@@ -17,4 +20,20 @@ export async function getBalance(req, res) {
         console.error(error.message);
         res.status(500).send(fail("Internal Server Error"));
     }
+}
+
+export async function createAccount(newId = null) {
+    allCustomers = await readCustomersFile();
+    if (!newId)
+        newId =
+            Math.max(0, ...allCustomers.map((custom) => custom.customerId)) + 1;
+    const initialBalance = 500;
+    const newCutomer = {
+        customerId: newId,
+        balance: initialBalance,
+        cart: [],
+    };
+    allCustomers.push(newCutomer);
+    await writeCustomersFile(allCustomers);
+    console.log("add new customer to customers.json");
 }
